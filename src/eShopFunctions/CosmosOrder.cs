e308aa93c21a47fe818610a9858007b0f1b30291
anton_yurchenko <anton.yurchenko@gmail.com>
2022-10-19 09:49:51 +0300

    save order to cosmosdb

---
+++ src/eShopFunctions/CosmosOrder.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopFunctions;
+public class CosmosOrder
+{
+    public int Id { get; set; }
+    public string BuyerId { get; set; }
+    public Address ShipToAddress { get; set; }
+    public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+
+    public IEnumerable<OrderItem> OrderItems = new List<OrderItem>();
+
+    public decimal Total()
+    {
+        var total = 0m;
+        foreach (var item in OrderItems)
+        {
+            total += item.UnitPrice * item.Units;
+        }
+        return total;
+    }
+
+}
+
+public class CosmosOrderExt
+{
+    public string id { get; set; }
+
+    public string Id { get; set; }
+    public int OrderId { get; set; }
+    public string BuyerId { get; set; }
+    public Address ShipToAddress { get; set; }
+    public DateTimeOffset OrderDate { get; set; }
+
+    public IEnumerable<OrderItem> OrderItems;
+
+    public CosmosOrderExt(CosmosOrder data)
+    {
+        id = Guid.NewGuid().ToString().ToUpper();
+        Id = id;
+        if (data != null)
+        {
+            OrderId = data.Id;
+            BuyerId = data.BuyerId;
+            ShipToAddress = data.ShipToAddress;
+            OrderDate = data.OrderDate;
+            OrderItems = data.OrderItems;
+        }
+    }
+
+    public decimal Total()
+    {
+        var total = 0m;
+        foreach (var item in OrderItems)
+        {
+            total += item.UnitPrice * item.Units;
+        }
+        return total;
+    }
+
+}
+
+public class Address // ValueObject
+{
+    public string Street { get; private set; }
+
+    public string City { get; private set; }
+
+    public string State { get; private set; }
+
+    public string Country { get; private set; }
+
+    public string ZipCode { get; private set; }
+
+    private Address() { }
+
+    public Address(string street, string city, string state, string country, string zipcode)
+    {
+        Street = street;
+        City = city;
+        State = state;
+        Country = country;
+        ZipCode = zipcode;
+    }
+}
+
+public class OrderItem 
+{
+    public int Id { get; set; }
+    public CatalogItemOrdered ItemOrdered { get; private set; }
+    public decimal UnitPrice { get; private set; }
+    public int Units { get; private set; }
+
+    private OrderItem()
+    {
+        // required by EF
+    }
+
+    public OrderItem(CatalogItemOrdered itemOrdered, decimal unitPrice, int units)
+    {
+        ItemOrdered = itemOrdered;
+        UnitPrice = unitPrice;
+        Units = units;
+    }
+}
+
+public class CatalogItemOrdered // ValueObject
+{
+    public int CatalogItemId { get; private set; }
+    public string ProductName { get; private set; }
+    public string PictureUri { get; private set; }
+}
+
--- src/eShopFunctions/OrderInfo.cs
+++ src/eShopFunctions/OrderInfo.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -8,11 +8,24 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Azure.Storage.Blobs;
+using Microsoft.Azure.Cosmos;
+using System.Collections.Concurrent;
 
 namespace eShopFunctions
 {
     public static class OrderInfo
     {
+        private static CosmosClient cosmosClient;
+
+        static OrderInfo()
+        {
+            if (cosmosClient == null)
+            {
+                string cosmosDBConnectionString = Environment.GetEnvironmentVariable("AzureCosmosDBConnectionString");
+                cosmosClient = new CosmosClient(cosmosDBConnectionString);
+            }
+        }
+
         [FunctionName("OrderInfo")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
@@ -25,12 +38,32 @@ namespace eShopFunctions
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             //dynamic data = JsonConvert.DeserializeObject(requestBody);
             //name = name ?? data?.name;
-            var storageConnection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            
+            /*var storageConnection = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
             BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnection);
             BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("orders");
             await containerClient.CreateIfNotExistsAsync();
             BlobClient blobClient = containerClient.GetBlobClient(orderId);
-            await blobClient.UploadAsync(new BinaryData(requestBody));
+            await blobClient.UploadAsync(new BinaryData(requestBody));*/
+
+
+            //CosmosDB save
+            var container = cosmosClient.GetContainer("eShopOrderDB", "OrderInfo");
+            CosmosOrder data = JsonConvert.DeserializeObject<CosmosOrder>(requestBody);
+            CosmosOrderExt dataExt = new CosmosOrderExt(data);
+
+            try
+            {
+                var result = await container.CreateItemAsync(dataExt, new PartitionKey(dataExt.Id));
+                return new OkObjectResult(result.Resource.id);
+            }
+            catch (CosmosException cosmosException)
+            {
+                log.LogError("Creating item failed with error {0}", cosmosException.ToString());
+                return new BadRequestObjectResult($"Failed to create item. Cosmos Status Code {cosmosException.StatusCode}, Sub Status Code {cosmosException.SubStatusCode}: {cosmosException.Message}.");
+            }
+
+
 
             string responseMessage = string.IsNullOrEmpty(orderId)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
